04527556bb5ffb06bb849b13aea56a75bfe84338
Vihaan <vihaanverma21@gmail.com>
2018-01-01 16:00:34 +0530

    create

---
+++ code/src/main/java/com/vihaan/code/creatingObservables/Create.java
@@ -0,0 +1,55 @@
+package com.vihaan.code.creatingObservables;
+
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Created by vihaanverma on 01/01/18.
+ */
+
+public class Create {
+
+
+    public static void main(String[] args) {
+
+        Observable<Integer> integerObservable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+
+                for(int i=0;i<10;i++)
+                {
+                    e.onNext(i);
+                }
+                e.onComplete();
+            }
+        });
+
+        integerObservable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe: " + d.toString());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete: " );
+            }
+        });
+    }
+
+
+}
