be4b49f265e5a8afea2b26c703196add74768095
Vihaan <vihaanverma21@gmail.com>
2018-01-02 00:17:10 +0530

    flowable test

---
+++ code/src/main/java/com/vihaan/code/flowable/FlowableTest.java
@@ -0,0 +1,109 @@
+package com.vihaan.code.flowable;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.reactivex.Flowable;
+import io.reactivex.FlowableSubscriber;
+import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.operators.flowable.FlowableLastMaybe;
+
+/**
+ * Created by vihaanverma on 01/01/18.
+ */
+
+public class FlowableTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        test();
+    }
+
+    private static void test() throws InterruptedException {
+        int [] numbers = new int[]{1,2,3,4,5,6,7,8,9,10};
+        Flowable<Integer> just = Flowable.just(1, 2, 3, 4, 5, 6);
+
+
+
+        Subscriber<Integer> subscriber= new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                System.out.println();
+                s.request(10);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println();
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.out.println();
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println();
+
+            }
+        };
+
+
+        just.subscribe(subscriber);
+
+
+        Single<List<Integer>> listSingle = just.toList();
+        listSingle.subscribe(new SingleObserver<List<Integer>>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onSuccess(List<Integer> integers) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        });
+
+        listSingle.toFlowable()
+                .subscribe(new Subscriber<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+
+                    }
+
+                    @Override
+                    public void onNext(List<Integer> integers) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+        Thread.sleep(1000*10);
+
+    }
+
+
+}
